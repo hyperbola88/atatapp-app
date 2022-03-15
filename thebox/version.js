713f4c61b2fc83b70fb16d79d70d0419a8f3deb8
Evgeny Prozorov <evgeny.p@nakukop.com>
2022-03-15 21:36:42 +0100

    added version

---
+++ thebox/version.js
@@ -0,0 +1,6 @@
+version="1.0.0";
+libs = [
+    'https://cdn.jsdelivr.net/npm/@orange-games/phaser-cachebuster/build/phaser-cachebuster.js',
+    'https://cdn.jsdelivr.net/npm/@orange-games/phaser-spine/build/phaser-spine.js',
+    'https://cdn.jsdelivr.net/npm/@orange-games/phaser-super-storage/build/phaser-super-storage.min.js'
+];
